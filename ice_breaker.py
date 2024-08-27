f332cadbe1e7a9c144ce011d22c97f25fba2201b
Jonathan Saddler <jonathan.saddler@ibm.com>
2024-08-27 08:09:50 -0400

    [STEP2] Refactor into functions

--- ice_breaker.py
+++ ice_breaker.py
@@ -6,14 +6,9 @@ from langchain.chains import LLMChain
 from third_parties.linkedin import scrape_linkedin_profile
 
 
-
-
-if __name__ == '__main__':
-    load_dotenv()
-    
-    print('Hello Langchain')
-    # print(os.environ['OPENAI_API_KEY'])
-    print(f'Cool API Key {os.environ["COOL_API_KEY"]}')
+def ice_break_with(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
     
     summary_template = """
     given the information {information} about a person I want you to create: 
@@ -25,10 +20,16 @@ if __name__ == '__main__':
     summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
 
     llm = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo')
-    
+
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/jonathan-a-saddler")
     
     res = chain.invoke(input={"information": linkedin_data})
     
-    print(res)
+    print(res)    
+
+if __name__ == '__main__':
+    load_dotenv()
+    print("Ice Breaker Enter")
+    ice_break_with(name="Eden Marco")
+    
