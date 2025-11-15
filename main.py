b9cbcb78477ef2080410161c05c192b33b26f8f4
jazad136 <1129384+jazad136@users.noreply.github.com>
2025-11-15 08:23:42 -0500

    Add inspect TavilySearch tool to main.py for inspection

--- main.py
+++ main.py
@@ -11,9 +11,23 @@ from langchain_classic.agents.react.agent import create_react_agent
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
+tools = TavilySearch()
+# '''A search engine optimized for comprehensive, accurate, and 
+# trusted results. Useful for when you need 
+# to answer questions about current events. 
+# It not only retrieves URLs and snippets, 
+# but offers advanced search depths, domain management,
+# time range filters, and image search, this tool 
+# delivers real-time, accurate, and citation-backed 
+# results.Input should be a search query.'''
 def main():
     print("Hello from langchain-course!")
-
+# tools.query
+# '''
+# {'description': 'Search query to look up', 
+# 'title': 'Query', 
+# 'type': 'string'}
+# '''
 
 if __name__ == "__main__":
     main()
